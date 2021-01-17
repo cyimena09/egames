ecf45f21d8d682b1e5fdd45e5c303383b2257c17
Emile Cyimena <69576607+cyimena09@users.noreply.github.com>
2021-01-17 21:21:28 +0100

    Update team_signup.php

--- controllers/team/team_signup.php
+++ controllers/team/team_signup.php
@@ -11,7 +11,7 @@ if (isset($_GET['player'])) {
 } else {
     $player = 0;
 }
-$newPlayer = $player + 1; // les joueurs sont encodé 1 à 1, 'newPlayer' correspond au joueur suivant
+$newPlayer = $player + 1; // les joueurs sont encodés 1 à 1, 'newPlayer' correspond au joueur suivant
 
 // --------------------------------------------------------------------------------------------------
 /* Opération d'insertion dans la base de données  de l'équipe et des joueurs */
@@ -21,7 +21,7 @@ $error = true;
 htmlSpecialArray($_POST);
 checkTrimArray($_POST);
 // Si on arrive dans ce bloc de code c'est qu'on a fini d'encoder les joueurs
-// et qu'une fois l'équipe et le jeu encodé, l'inscription sera terminée.
+// et qu'une fois l'équipe et le jeu encodés, l'inscription sera terminée.
 if (isset($_POST['game']) && isset($_POST['teamName'])) {
     $game = $_POST['game'];
     $teamName = $_POST['teamName'];
@@ -40,12 +40,12 @@ if (isset($_POST['game']) && isset($_POST['teamName'])) {
         // n'est accessible que si le nombre de joueur valide à été respecté.
         if (count($_SESSION['players']) == $maxPlayer) {
             foreach ($players as $player) {
-                $playerId = createPlayer($player, $teamId); // On insert le joueur et la clé étrangère de son équipe
+                $playerId = createPlayer($player, $teamId); // On insert le joueur et l'id de son équipe dans la colonne FK_Team (clé étrangère).
                 insertParticipation($playerId, $teamId, $game); // On insert l'id du joueur et de son équipe dans la table 'participations'.
                 // On envoie un mail à chaque joueur pour confirmer leur inscription.
                 sendConfirmMail($player['email']);
             }
-            // une fois l'inscription cloturé on détruit la session et on redirige vers une page de succès
+            // une fois l'inscription cloturée on détruit la session et on redirige vers une page de succès
             session_destroy();
             header("Location: ../../views/success_signup.php");
         } else {
@@ -84,7 +84,7 @@ else {
             header("Location: ../../views/team_signup.php?player=$player&error=$error");
         } else {
             // Une fois que les données sont dans un format correct, on vérifie que l'utilisateur n'a pas encodé
-            // deux fois la même addresse
+            // deux fois la même adresse mail
             $similitude = 0;
             for ($i = 0; $i < count($_SESSION['players']); $i++) {
                 if ($_POST['email'] == $_SESSION['players'][$i]['email'] && $i != $player) {
@@ -106,7 +106,7 @@ else {
 }
 
 // --------------------------------------------------------------------------------------------------
-/* Si les données sont correct ou peut passer à l'insertion du joueur suivant */
+/* Si les données sont correctes on peut passer à l'insertion du joueur suivant */
 // --------------------------------------------------------------------------------------------------
 
 if ($error == false) {
